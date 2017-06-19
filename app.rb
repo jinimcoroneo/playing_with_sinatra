0eb05e4e7c3a31cae7116c243d9dca24669efffb
jinimcoroneo <jinim1983@gmail.com>
2017-06-19 16:33:12 +0100

    add params

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'shotgun'
 
-get '/' do
-  @name = ["Jini","Alistair"].sample
+get '/random_cat' do
+  # @name = ["Jini","Alistair"].sample
   erb(:index)
+end
+
+get '/named_cat' do
+  @name = params[:name]
+  erb :index
+end
+
+get '/colored_cat' do
+  @name = params[:name]
+  @color = params[:color]
+  @background = params[:background]
+  erb :index
 end
