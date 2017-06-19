45e8d740591bbc31cbee6169c3e7f4ead057c65e
Alistair Kung <alistaircckung@gmail.com>
2017-06-19 15:56:23 +0100

    added header to cat

--- app.rb
+++ app.rb
@@ -2,10 +2,5 @@ require 'sinatra'
 require 'shotgun'
 
 get '/' do
-  "testing"
-end
-get '/cat' do
-  "<div>
-    <img style='border:5px dashed red' src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
