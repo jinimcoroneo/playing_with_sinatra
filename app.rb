adeafda9e5068d5e9b307a1a903e4d6d9abca5a0
terminalobject <doctorboredom@hotmail.com>
2017-06-20 12:27:52 +0100

    added form

--- app.rb
+++ app.rb
@@ -8,7 +8,12 @@ end
 
 get '/named_cat' do
   @name = params[:name]
-  @color = params[:color]
-  @border_color = params[:border_color]
+  # @color = params[:color]
+  # @border_color = params[:border_color]
+  erb :index
+end
+
+get '/test' do
+  @name = params[:name]
   erb :index
 end
