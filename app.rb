4bd3ce9b5ff3efba7563216f64839809e3b5b900
jinimcoroneo <jinim1983@gmail.com>
2017-06-20 12:45:35 +0100

    post the params

--- app.rb
+++ app.rb
@@ -3,17 +3,17 @@ require 'shotgun'
 
 get '/random_cat' do
   # @name = ["Jini","Alistair"].sample
-  erb(:index)
-end
-
-get '/named_cat' do
-  @name = params[:name]
-  # @color = params[:color]
-  # @border_color = params[:border_color]
   erb :index
 end
 
-get '/test' do
+get '/naming_form' do
+  erb :index
+end
+
+post '/named_cat' do
   @name = params[:name]
+  p params
+  # @color = params[:color]
+  # @border_color = params[:border_color]
   erb :index
 end
